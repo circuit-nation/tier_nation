cbfb90caf4db4878ab49896f13c5435475f7c668
Pranav Tripathi <tripathipranav14@gmail.com>
2026-04-17 19:46:21 +0530

    remove: client files - monorepo setup

--- src/types/entity.ts
+++
@@ -1,7 +0,0 @@
-export type Entity = {
-  id: string
-  name: string
-  imageUrl?: string
-  team?: string
-  tags?: string[]
-}
--- src/types/index.ts
+++
@@ -1,5 +0,0 @@
-export type { Entity } from './entity'
-export type { List } from './list'
-export type { Vote } from './vote'
-export { TIER_VALUES } from './tier'
-export type { Tier, TierBoardState, TierValue } from './tier'
