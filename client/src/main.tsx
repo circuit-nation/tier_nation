701274e8e949b94fb5020c1af7f300482678401d
Pranav Tripathi <tripathipranav14@gmail.com>
2026-04-19 12:54:25 +0530

    feat: update favicon and logo assets; remove unused images; update font

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,6 +5,11 @@ import { RouterProvider } from 'react-router-dom'
 import { AppProviders } from '@/app/providers'
 import { router } from '@/app/router'
 
+// Font DM Sans
+import '@fontsource-variable/dm-sans/opsz-italic.css';
+import '@fontsource-variable/dm-sans/wght.css';
+import '@fontsource-variable/dm-sans/wght-italic.css';
+
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
