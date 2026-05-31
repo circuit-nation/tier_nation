b526e6a092e480e3d7a8a10a101f14e979b4c314
Pranav Tripathi <tripathipranav14@gmail.com>
2026-05-31 12:21:20 +0530

    fix: disable SWR revalidateOnFocus to prevent home page image flicker on tab switch

--- client/src/lib/swr/config.ts
+++ client/src/lib/swr/config.ts
@@ -1,6 +1,6 @@
 import type { SWRConfiguration } from 'swr';
 
 export const defaultSwrConfig: SWRConfiguration = {
-  revalidateOnFocus: true,
+  revalidateOnFocus: false,
   shouldRetryOnError: false,
 };
